45f497524880914cff425d4f51d98af93a679720
MUCHENG <3544306302>
2019-03-23 12:10:03 +0800

    modify

--- src/main/java/com/api/log/controller/LogController.java
+++ src/main/java/com/api/log/controller/LogController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class LogController {
